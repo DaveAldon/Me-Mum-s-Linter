36a685e321727280e6a28d5d1cc1d729664048b9
DaveAldon <crawford@mail.gvsu.edu>
2018-06-14 16:27:20 -0400

    Changed linter to handle everything as though it's one line, based on blocks.

--- linter.py
+++ linter.py
@@ -2,6 +2,8 @@ import syntax
 import re
 from os.path import abspath, exists
 
+blocks = []
+
 
 def awake():
     injest()
@@ -12,44 +14,43 @@ def injest():
     f_path = abspath("bad.cls")
     if exists(f_path):
         with open(f_path) as f:
-            content = f.readlines()
-    # Dump lines into a list
-    content = [x.strip() for x in content]
-    first_order(content)
+            content = f.read()
 
-def first_order(content):
+    # Blocks consist of Class definition, class block, and function sub blocks
+    content = content.split("{", 1)
+    blocks.append(content[0])
+    end_block = content[1].rsplit("}", 1)
+    blocks.append(end_block[0])
+
+    class_definition(blocks[0])
+    function_definitions(blocks[1])
+
+def function_definitions(content):
+    print(content)
+
+def class_definition(content):
     def first_order_optional(line):
         return line.startswith(syntax.first_order_optional)
     def first_order_required(line):
         return line.startswith(tuple(syntax.first_order))
 
     # Default states of blocks. Set to true once found
-    open_block = False
-    close_block = False
-
+    content = content.split('\n')
     for line in content:
         if first_order_optional(line): continue
         if first_order_required(line):
             line = line.split()
             pattern = re.compile(syntax.class_definition)
-            if pattern.match(line[1]): print("match definition")
+            if not pattern.match(line[1]): print("failed to match definition")
             if len(line) > 1:
                 if line[2].startswith('Extends'):
                     pattern = re.compile(syntax.class_extension)
-                    if pattern.match(line[3]): print("match extension")
+                    if not pattern.match(line[3]): print("failed to match extension")
                 else: print("Invalid Extend")
-            #TODO: Handling of [] params in extensions
-            try:
-                block_begin = line.index("{")
-                open_block = True
-                if (block_begin) < (len(line)-1):
-                    #TODO: This assumes there's a lot of logic in one line. We're not ready to tackle that
-                    print("Too complex for now!")
-            except:
-                break
-            break
 
-        else: print("fail")
+            #TODO: Handling of [] params in extensions
+            break
+        else: print("Bad comment or Class form")
 
 
 
